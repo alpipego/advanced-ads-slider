28dd60582d0e995a2874c08b497d289ad7e49293
Thomas Maier <post@webzunft.de>
2015-06-19 21:34:17 +0200

    change content of slider add-on widget on overview page if add-on is installed

--- admin/admin.php
+++ admin/admin.php
@@ -9,6 +9,8 @@ class Advanced_Ads_Slider_Admin {
 	 */
 	protected $plugin;
 
+	const PLUGIN_LINK = 'http://wpadvancedads.com/add-ons/slider/';
+
 	/**
 	 * Initialize the plugin by loading admin scripts & styles and adding a
 	 * settings page and menu.
@@ -20,6 +22,9 @@ class Advanced_Ads_Slider_Admin {
 		$this->plugin = Advanced_Ads_Slider_Plugin::get_instance();
 		// add group options
 		add_action( 'advanced-ads-group-form-options', array( $this, 'group_options' ) );
+
+		// add snippet to overview page
+		add_action('advanced-ads-admin-overview-before', array($this, 'register_overview_page_widget'), 10, 2);
 	}
 
 	/**
@@ -33,4 +38,26 @@ class Advanced_Ads_Slider_Admin {
 
 		include AAS_BASE_PATH . 'admin/views/group-options.php';
 	}
+
+	/**
+	* update the widget on the overview page
+	*
+	* @since 1.0.3
+	*/
+       public function register_overview_page_widget(){
+	   global $wp_meta_boxes;
+
+	   // change the callback of the widget
+	   $wp_meta_boxes['toplevel_page_advanced-ads']['side']['high']['advads_overview_addon_slider']['callback'][0] = 'Advanced_Ads_Slider_Admin';
+	   $wp_meta_boxes['toplevel_page_advanced-ads']['side']['high']['advads_overview_addon_slider']['callback'][1] = 'render_overview_widget';
+       }
+
+       /**
+	* render infos on overview page
+	*
+	* @since 1.1.3
+	*/
+       public static function render_overview_widget(){
+	   require_once( 'views/overview.php' );
+       }
 }
