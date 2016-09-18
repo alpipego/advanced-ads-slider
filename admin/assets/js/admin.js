0beae52abd5060ef5293dc6c40598de0a3f7ad6e
Thomas Maier <post@webzunft.de>
2016-09-18 15:06:47 +0200

    optimized admin js to allow other group types

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -2,11 +2,12 @@ jQuery(document).ready(function($){
 	$('.advads-ad-group-type input').click(function(){
 	    var slider_options = $(this).parents('.advads-ad-group-form').find('.advads-group-slider-options');
 	    var number_option = $(this).parents('.advads-ad-group-form').find('.advads-ad-group-number');
+	    slider_options.hide();
 	    if( 'slider' === $(this).val() ) {
 			slider_options.show();
 			// set number to all and hide setting
 			number_option.val('all').hide();
-	    } else {
+	    } else if( 'default' === $(this).val() || 'ordered' === $(this).val() ) {
 			slider_options.hide();
 			number_option.show();
 	    }
